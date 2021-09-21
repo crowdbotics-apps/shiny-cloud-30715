1e71fcf7562f7b1f0054100692fd6902ba68178b
Crowdbotics <team@crowdbotics.com>
2021-09-21 01:41:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shiny_cloud_30715Identifier",
+  url: "https://shiny_cloud_30715Identifier.botics.co",
+  api: "https://shiny_cloud_30715Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
